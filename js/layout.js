2756f6c7b0901dfbec0e7f3751eb36eefd6537f8
Cassio Nazare <cassionazare.estudos@gmail.com>
2018-12-10 22:58:33 -0200

    Aula1

--- js/layout.js
+++ js/layout.js
@@ -1,7 +1,5 @@
 var botaoLinhaBloco = document.querySelector("#btnMudaLayout")
 
-botaoLinhaBloco.onclick = alteraNome
-
 function alteraNome() {
     if (botaoLinhaBloco.textContent === "Blocos") {
         botaoLinhaBloco.textContent = "Linhas"
@@ -10,4 +8,17 @@ function alteraNome() {
     }
 }
 
-console.log("layout")
+var mural = document.querySelector(".mural")
+
+function mudaClasse() {
+    mural.classList.toggle("mural--linha")
+}
+
+/*
+botaoLinhaBloco.addEventListener("click", mudaClasse)
+botaoLinhaBloco.addEventListener("click", alteraNome)*/
+
+botaoLinhaBloco.addEventListener("click", function() {
+    mudaClasse()
+    alteraNome()
+})
